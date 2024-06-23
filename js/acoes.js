cb144cbb536b4b6b571f56383552226689b536de
fernandoaurelio <fernandoaurelios@gmail.com>
2024-06-23 16:01:18 -0300

    Melhoria do jquery e css responsivo, adicionado bootstrap, modal

--- js/acoes.js
+++ js/acoes.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
-	$(".toggle-menu").on("click", function(){
-		$("ul").css({"display": "block"});
-		$("i.fas.fa-bars").css({"display": "none"});	
-		$("i.fas.fa-times").css({"display": "block"});	
 	
+
+	$("a").on("click", function(){
+		$(".modal").modal("show");
 	});
-	$("i.fas.fa-times").on("click", function(){
-		$("ul").css({"display": "none"});
-		$("i.fas.fa-times").css({"display":"none"});
-		$("i.fas.fa-bars").css({"display":"block"});
+	$(".fas.fa-bars").on("click", function(){
+		$(".header-links ul").css({"display":"block"});
+		$(this).hide();
+		$(".fas.fa-times").css({"display":"block"});
 	});
+	$(".fas.fa-times").on("click", function(){
+		$(".header-links ul").css({"display":"none"});
+		$(this).hide();
+		$(".fas.fa-bars").css({"display":"block"});
+	});
+
+
 });
